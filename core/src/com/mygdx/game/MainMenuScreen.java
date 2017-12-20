692bd19759c50e07bf7d8483affb4cd75b1262fe
daniel <d.eden2@hotmail.com>
2017-12-20 13:24:50 +0000

    Added basic menu system with menu classes using Game and Screen interfaces.

---
+++ core/src/com/mygdx/game/MainMenuScreen.java
@@ -0,0 +1,95 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+
+public class MainMenuScreen implements Screen {
+
+    final MyGdxSimulation game;
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final MyGdxSimulation game) {
+        this.game = game;
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+
+
+    /**
+     * Called when this screen becomes the current screen for a Game.
+     */
+    @Override
+    public void show() {
+
+    }
+
+    /**
+     * Called when the screen should render itself.
+     *
+     * @param delta The time in seconds since the last render.
+     */
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to Natural Computing!!! ", 100, 150);
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    /**
+     * @param width
+     * @param height
+     *
+     */
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void pause() {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void resume() {
+
+    }
+
+    /**
+     * Called when this screen is no longer the current screen for a  Game.
+     */
+    @Override
+    public void hide() {
+
+    }
+
+    /**
+     * Called when this screen should release all resources.
+     */
+    @Override
+    public void dispose() {
+
+    }
+}
